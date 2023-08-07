f7054679b06f02171fd22f207969be100b5e3371
faraguti <faraguti@gmail.com>
2023-08-06 18:14:16 -0600

    Initial commit

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
 resource "aws_instance" "ec2-instance-1" {
   ami           = "ami-03f65b8614a860c29" ## Ubuntu Server 22.04 LTS (HVM), SSD Volume Type // us-west-2
   instance_type = "t2.micro"
